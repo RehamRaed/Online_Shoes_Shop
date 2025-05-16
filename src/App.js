c0c0bbb36a8098dd5be8fb2455ffb02a6fe71184
RehamRaed <r409257797@gmail.com>
2025-05-16 14:56:53 +0300

    Updated README or code

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const AppContent = () => {
   const [loading, setLoading] = useState(false); 
 
   useEffect(() => {
-    setLoading(true); 
+    setLoading(false); 
     const timer = setTimeout(() => setLoading(false), 2000);
 
     return () => clearTimeout(timer); 
